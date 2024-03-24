a38fddedd06ffd0da40d15b7bad8e09b469e5df5
Stitch <stitch@s19.be>
2024-03-24 19:44:32 +0100

    new try

---
+++ Inception/NEW_INCEPTION/srcs/requirements/wordpress/conf/entrypoint.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+sleep 15
+
+if [ ! -f "/var/www/wordpress/wp-config.php" ]; then
+	wp core download \
+					--path='/var/www/html' \
+					--allow-root
+
+    wp config create --allow-root \
+					--dbhost=$MARIADB_HOST \
+    				--dbname=$MARIADB_NAME \
+    				--dbuser=$MARIADB_USER \
+    				--dbpass=$MARIADB_PASSWORD \
+					--path='/var/www/wordpress'
+
+	wp core install \
+					--path=/var/www/html \
+					--url=$WORDPRESS_URL \
+					--title=$WORDPRESS_TITLE \
+					--admin_user=$WORDPRESS_ROOT_USER \
+					--admin_password=$WORDPRESS_ROOT_PASSWORD \
+					--admin_email=$WORDPRESS_ROOT_EMAIL \
+					--allow-root
+	
+	wp user create \
+				$WORDPRESS_USER $WORDPRESS_USER_EMAIL \
+				--user_pass=$WORDPRESS_USER_PASSWORD \
+				--role=author --path=/var/www/wordpress \
+				--allow-root
+fi
+
+if [ ! -d /run/php ]; then
+    mkdir -p /run/php
+fi
